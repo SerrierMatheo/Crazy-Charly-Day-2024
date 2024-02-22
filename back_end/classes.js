42e45a4f1031616367c17eb8092ba68321fa9aee
perez112u <coganperez@gmail.com>
2024-02-22 19:02:21 +0100

    ajout optimisation

--- back_end/classes.js
+++ back_end/classes.js
@@ -1,9 +1,24 @@
 export class Candidat {
     constructor(nom, preferences, nombreAteliers) {
         this.nom = nom;
+        this.nbAffect = 0;
         // tableau des preferences decroissantes
         this.preferences = preferences;
         this.nombreAteliers = nombreAteliers;
+    }
+
+    getScorePreference(theme) {
+        let score = 10;
+        for (let preference of this.preferences) {
+            if (preference === theme) {
+                return score;
+            } else {
+                score -= 2;
+                if (score < 2) {
+                    score = 1;
+                }
+            }
+        }
     }
 }
 
@@ -13,17 +28,52 @@ export class Atelier {
         this.theme = theme;
         this.placesDisponibles = placesDisponibles;
     }
+    affecter() {
+        this.placesDisponibles--;
+    }
+
 }
 
 export class Affectation {
     constructor() {
         this.affectations = {};
+        this.nbAffectation = 0;
     }
 
-    ajouterAffectation(candidat, atelier) {
-        if (!this.affectations[atelier.id]) {
-            this.affectations[atelier.id] = [];
+    ajouterAffectation(candidat, idAtelier) {
+        if (!this.affectations[idAtelier.id]) {
+            this.affectations[idAtelier.id] = [];
         }
-        this.affectations[atelier.id].push(candidat);
+        this.affectations[idAtelier.id].push(candidat);
+        this.nbAffectation++;
+        candidat.nbAffect++;
+    }
+    estAffecte(id) {
+        if (this.affectations[id]) {
+            return true;
+        }
+        return false;
+    }
+    nbAffectationCandidat(nom) {
+        let res = 0;
+        for (let candidats of this.affectations) {
+            for (let candidat of candidats) {
+                if (candidat.nom === nom) {
+                    res++;
+                }
+            }
+
+        }
+        return res;
+    }
+
+    copieAffectation() {
+        let copy = new Affectation();
+
+        for (let idAtelier in this.affectations) {
+            copy.affectations[idAtelier] = [...this.affectations[idAtelier]];
+        }
+        copy.nbAffectation = this.nbAffectation;
+        return copy;
     }
 }
--- back_end/script.js
+++ back_end/script.js
@@ -2,9 +2,132 @@ import {Candidat, Atelier, Affectation} from "./classes.js";
 
 let candidat1 = new Candidat("Albert", ["JP", "FR", "OR", "MEX", "IT", "GR"], 3);
 let candidat2 = new Candidat("Bertrand", ["OR", "JP", "FR", "IT", "MEX", "GR"], 3);
-let atelier1 = new Atelier("JP", 2);
-let atelier2 = new Atelier("OR", 2);
+let candidats = [candidat1, candidat2];
+let atelier1 = new Atelier(1,"JP", 2);
+let atelier2 = new Atelier(2,"OR", 2);
+let ateliers = [atelier1, atelier2];
 
-let affectation = new Affectation();
-affectation.ajouterAffectation(candidat1, atelier1);
-affectation.ajouterAffectation(candidat2, atelier2);
+let nbAteliersImp = 0;
+
+
+function getAtelierDispo(theme) {
+    for(let atelier of ateliers) {
+        if (atelier.theme === theme) {
+            if (!affectation.estAffecte(atelier.id) && atelier.placesDisponibles > 0) {
+                return atelier;
+            }
+        }
+    }
+    return undefined;
+}
+
+function trouverMeilleurScore() {
+    let meilleurScore = -1;
+    let meilleurAtelier;
+    let meilleurCandidat;
+
+    // on parcourt chaque candidat
+    for (let candidat of candidats) {
+        let i = 10;
+        let score;
+        let atelier;
+        if (candidat.nombreAteliers > affectation.nbAffectationCandidat(candidat.nom)) {
+
+            // on parcours chaque preference du candidat (triée)
+            for (let theme of candidat.preferences) {
+                atelier = getAtelierDispo(theme);
+                // si un atelier du theme courant existe on calcule son score
+                if (atelier !== undefined) {
+                    score = i;
+                    if (score > meilleurScore) {
+                        meilleurScore = score;
+                        meilleurAtelier = atelier;
+                        meilleurCandidat = candidat;
+                    }
+                }
+                // on augmente le malus
+                i -= 2;
+                if (i < 2) {
+                    i = 1;
+                }
+            }
+        }
+    }
+    return [meilleurCandidat, meilleurAtelier];
+}
+
+
+function calculerScore(affectationCourante) {
+    let score = 0;
+    for (let idAtelier in affectationCourante.affectations) {
+        let atelier;
+        for (let a of ateliers) {
+            if (a.id == idAtelier) {
+                atelier = a;
+            }
+        }
+        for (let candidat of affectationCourante.affectations[idAtelier]) {
+            // + gerer le malus
+            score += candidat.getScorePreference(atelier.theme);
+        }
+        // + gerer les personne non affecté
+    }
+    return score;
+}
+
+function algoGloutonRecursif (candidatsRestants, ateliersRestants, affectationCourante) {
+    if (candidatsRestants.length === 0 || ateliersRestants.length === 0) {
+        let score = calculerScore(affectationCourante);
+        console.log("score")
+        return {affectation: affectationCourante, score: score};
+    }
+
+    let meilleurResultat = {affectation: [], score: -1};
+
+    for (let candidat of candidatsRestants) {
+        if (candidat.nombreAteliers > candidat.nbAffect) {
+
+            for (let atelier of ateliersRestants) {
+                if (atelier.placesDisponibles > 0) {
+
+                    let copieAffectation = affectationCourante.copieAffectation()
+                    copieAffectation.ajouterAffectation(candidat, atelier);
+
+                    let nouveauxCandidatsRestants = candidatsRestants.filter(c => c !== candidat);
+                    let nouveauxAteliersRestants = ateliersRestants.filter(a => a !== atelier);
+                    let resultat = algoGloutonRecursif(nouveauxCandidatsRestants, nouveauxAteliersRestants, copieAffectation);
+
+                    if (resultat.score > meilleurResultat.score) {
+                        console.log(resultat.score)
+                        meilleurResultat = resultat;
+                    }
+                }
+            }
+        }
+    }
+
+    return meilleurResultat;
+
+
+}
+
+// while (nbAteliersImp < ateliers.length) {
+//     // selectionner la meilleur option disponible
+//     let optionMeilleurScore = trouverMeilleurScore(affectation);
+//     console.log(optionMeilleurScore)
+//
+//     //on affecte la solution
+//     let candidat = optionMeilleurScore[0];
+//     let atelier = optionMeilleurScore[1];
+//     affectation.ajouterAffectation(candidat, atelier.id);
+//     atelier.affecter();
+//     nbAteliersImp++;
+//
+// }
+let affect = new Affectation();
+let res = algoGloutonRecursif([...candidats], [...ateliers], affect);
+console.log("Score :")
+console.log(res[1]);
+console.log("Affectations :")
+console.log(res[0]);
+
